abc5e07fc9381105596f25f4557b2a6eb08f2a96
Omar Elgendy <71668450+Omar-m-elgendy@users.noreply.github.com>
2026-05-10 23:46:41 +0300

    Add files via upload
    integrate node.js & bd

--- index.js
+++ index.js
@@ -1,108 +1,60 @@
-/**
- * core module
- * custom module
- * third party module 
- */
-
-const fs = require ("node:fs");
-const path = require("node:path");
 const express = require("express");
-const anything = require("./utils.js");
-//anything.sayHi("Omar");
-
+const mysql = require("mysql2");
 const app = express();
 const port = 3000;
+// connection to DB
 
-//url method >> handler
-//get product
-
-/**
- * @method GET
- * @url /product
- * @params none
- * 
- */
-app.get("/product", (req, res, next) => {
-    let  data = fs.readFileSync("./product.json", "utf-8");
-    data = JSON.parse(data);
-    // logic of code
-    //res.setHeader("Content-Type", "application/json");
-    //res.write(JSON.stringify(data));
-    //res.end();
-
-    //logic of code
-    /**
-     * res.send
-     * res.json
-     * res.status   >> customize status code
-     * res.sendFile
-     */
-    //res.status(201).json(data);
-    res.sendFile(path.resolve("./product.json"));
-
-} );
-/**
- * @method POST
- * @url /product
- */
+const dbConnection = mysql.createConnection({
+    host: "localhost",  //  127.0.0.1  >> clever cloud >> 500m for free
+    //by default: 3306,
+    user: "root",
+    password: "",
+    database: "uber"
+});
+// check connection
 
 
-app.post("/product",
-    (req,res, next) => {
-        let body = "";
-        req.on("data", (chunk) => {
-            body += chunk;
-        });
-        req.on("end", () => {
-            //pares body 
-            req.body = JSON.parse(req.body);
-            req.body = body;
-            next();
-        });
-    },
-    (req, res,next) => { 
-    let body = "";
-    req.on("data", (chunk) => {
-        body += chunk;
-    });
-    req.on('end', () => {
-        body = JSON.parse(body);
-        console.log(body);
-        const data = fs.readFileSync("./product.json", "utf-8");
-        // add new data  >> body
-        data = JSON.parse(data);
-        data.push(req.body);
-        fs.writeFileSync("./product.json", JSON.stringify(data));
-        res.json({message: "Product added successfully", success:true});
+dbConnection.connect((err) => {
+    if (err) {
+        console.error("fail to connect to database: ", err);
+    } else {
+        console.log(" db connected to database successfully");
+    }
+});
+// builtin middleware express.json() >> parsing body >> row json
+
+app.use(express.json()); // global middleware >> for all routes
+
+//API >> add user
+app.post("/users", (req, res, next) => {
+    //get data from request body
+    const { name, email, password } = req.body;
+    // prepare query to execute in DB
+    let query = `INSERT INTO drivers (name, email, password) VALUES ("${name}", "${email}", "${password}")`;
+    // prepare statment
+    dbConnection.execute(query, (error, results) => {
+        if (error) {
+            if (error.errorno === 1062) {
+                return res.status(409).json({ message: "email already exists", success: false });
+            }
+            return res.status(500).json({ message: "server error", error });
+        }
+        if (results.affectedRows == 0) {
+            return res.status(500).json({ message: "fail to create user", success: false });
+        }
+        //send response
+        return res.status(201).json({ message: "user created successfully", success: true,userId: results.insertId });
+    //console.log({name, email, password}); // to make sure data is received correctly
+        //const query = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
     });
 });
 
-
-// consept midleware , is a function
-//app.post(
-//    "/dkg",
-    /** 
-    (req,res,next) => {
-        req.username = "john"; //>> middleware >> is a function that takes req, res, next and do something with them
-        if (1) next();
-        console.log(1);  // login
-    },  //>> middleware
-    (req,res,next) => {
-        console.log(req.username); //>> middleware
-        next();
-        console.log(2):  // is admin
-        },  //>> middleware
-    (req,res,next) => {
-        next();
-        console.log(3);  // phtos upload
-    },  //>> middleware
-    */
-//    (req,res,next) => {}  //>> handler
-//);
-// last next we don't need to call it because it's the last function in the chain
-//global error middleware
-
-
-app.listen(3000, () => {
-    console.log("Server is running on port ", port);
+/*
+// define a route
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+*/
+app.listen(port, () => {
+    console.log("App is running on port ", port);
 });
